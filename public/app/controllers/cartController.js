817a3726c3a4ece727c1af0aed0d837da9bbf7b1
Robin <Robin.edbom@gmail.com>
2016-03-15 14:00:55 +0100

    All Toast (errors and other info to the user) now goes to the ToastFactory to be displayed

--- public/app/controllers/cartController.js
+++ public/app/controllers/cartController.js
@@ -1,24 +1,16 @@
 var app = angular.module("myApp");
 
-app.controller('cartController', [ '$scope', '$http', 'CartFactory', 'UserFactory', 'Articles', function ( $scope, $http, cart, userFactory, Articles ) {
+app.controller('cartController', [ '$scope', '$http', 'CartFactory', 'UserFactory', 'articles', 'ToastFactory', function ( $scope, $http, cart, userFactory, articles, Toast ) {
 
+    var strNotLoggedIn = "You are not logged in, please login!";
     //get the token aka user id
     var token = userFactory.getUserInfo().token;
 
-    //set the article
-    $scope.items = Articles.query();
-
-    getMyItems(token, cart, function ( items ) {
-        $scope.myItems = items;
-    });
-
     $scope.addItem = function ( article ) {
-        if ( !token ) return Materialize.toast("You are not logged in, please login!");
+        if ( !token ) Toast.showError(strNotLoggedIn);
 
         cart.addItem(article, token, function ( err, updatedCart ) {
-            if ( err ) {
-                return showError(err);
-            }
+            if ( err ) return Toast.showError;
             $scope.myItems = updatedCart.items;
         })
 
@@ -26,12 +18,10 @@ app.controller('cartController', [ '$scope', '$http', 'CartFactory', 'UserFactor
 
 
     $scope.removeItem = function ( item ) {
-        if ( !token ) return Materialize.toast("You are not logged in, please login!");
+        if ( !token ) Toast.showError(strNotLoggedIn);
 
         cart.removeItem(item, token, function ( err, updatedCart ) {
-            if ( err ) {
-                return showError(err);
-            }
+            if ( err ) return Toast.showError;
 
             console.log(updatedCart);
             $scope.myItems = updatedCart.items;
@@ -42,35 +32,53 @@ app.controller('cartController', [ '$scope', '$http', 'CartFactory', 'UserFactor
 
 
     $scope.getMyCart = function () {
-        if ( !token ) return Materialize.toast("You are not logged in, please login!");
+        if ( !token ) Toast.showError(strNotLoggedIn);
 
-        getMyItems(token, cart, function ( items ) {
+        getMyItems(token, Toast, cart, function ( items ) {
             $scope.myItems = items;
         });
     };
 
 
     $scope.emptyMyCart = function () {
-        if ( !token ) return Materialize.toast("You are not logged in, please login!");
+        if ( !token ) Toast.showError(strNotLoggedIn);
 
         cart.emptyMyCart(token, function ( err, updatedCart ) {
-            if ( !token ) return Materialize.toast("You are not logged in, please login!");
+            if ( err ) return Toast.showError;
 
             $scope.myItems = updatedCart.items;
         });
-    }
+    };
+
+    $scope.initCollapsible = function () {
+        $(document).ready(function () {
+
+            console.log("done");
+        });
+    };
+
+    //does not work?
+    $scope.initCollapsible();
+
+    //load my items
+    $scope.getMyCart();
+
+    //set the article
+    articles.getAllArticles(function ( err, data ) {
+        if ( err ) return Toast.showError(err);
+
+        $scope.items = data;
+        //init all the modals after we fetched articles
+        $('.modal-trigger').leanModal();
+    });
 
 
 } ]);
 
-function showError( err ) {
-    Materialize.toast(err.Error);
-    console.warn(err.Error);
-}
 
-function getMyItems( token, cart, callback ) {
+function getMyItems( token, Toast, cart, callback ) {
     cart.getMyCart(token, function ( err, updatedCart ) {
-        if ( err ) return showError(err);
+        if ( err ) return Toast.showError;
 
         callback(updatedCart.items);
     });
