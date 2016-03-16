20b046a921d0ad98744bb84401c26427cbba00cb
Robin <Robin.edbom@gmail.com>
2016-03-16 14:24:27 +0100

    Added a file for company info

---
+++ resources/CompanyInfo.js
@@ -0,0 +1,11 @@
+var companyName = "Cash-It";
+var companyPhoneNumber = "054-24 75 50";
+var companyOrganisationNumber = "556575-5716";
+var companyWebsite = "http://www.cash-it.se";
+
+module.exports = {
+    name: companyName,
+    phoneNum: companyPhoneNumber,
+    orgNum: companyOrganisationNumber,
+    site: companyWebsite
+};
