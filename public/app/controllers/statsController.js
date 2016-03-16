5548c45091d8ee36d70a19caa0eb9138f4e08889
Robin <Robin.edbom@gmail.com>
2016-03-16 13:01:37 +0100

    Made some graphs in graph.js

--- public/app/controllers/statsController.js
+++ public/app/controllers/statsController.js
@@ -4,25 +4,37 @@ var app = angular.module('myApp');
 app.controller('statsController', [ '$scope', 'StatsFactory', 'ToastFactory', function ( $scope, stats, Toast ) {
 
 
-    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-    $scope.series = ['Series A', 'Series B'];
-    $scope.data = [
-        [65, 59, 80, 81, 56, 55, 40],
-        [28, 48, 40, 19, 86, 27, 90]
-    ];
-    $scope.onClick = function (points, evt) {
-        console.log(points, evt);
-    };
-
     $scope.getAllInfo = function () {
         console.log("fetching....");
         stats.getAllInfo(null, function ( err, data ) {
             if ( err ) return Toast.showError(err);
 
-            $scope.data = data;
+
+            $scope.barLabels = [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday' ];
+            $scope.barSeries = [ 'Total Sales' ];
+
+            $scope.barData = [
+                [
+                    data.totalSold * randomNumber(0.1, 1.9),
+                    data.totalSold * randomNumber(0.1, 1.9),
+                    data.totalSold * randomNumber(0.1, 1.9),
+                    data.totalSold * randomNumber(0.1, 1.9),
+                    data.totalSold * randomNumber(0.1, 1.9)
+                ]
+            ];
+
+
+            $scope.doughnutLabels = [ data.salesPersons[ 0 ].name, data.salesPersons[ 1 ].name ];
+            $scope.doughnutData = [ data.salesPersons[ 0 ].totalSold, data.salesPersons[ 1 ].totalSold ]
+
+
         });
     };
 
     $scope.getAllInfo();
 
 } ]);
+
+function randomNumber( max, min ) {
+    return (Math.random() * (max - min) + min)
+}
