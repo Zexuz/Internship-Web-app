cde28ea4ca024543422990895267645ce426d902
Robin <Robin.edbom@gmail.com>
2016-04-04 15:45:16 +0200

    Added GET /Items/Search?ean=X&sku=y REST end point

--- routes/itemRouter.js
+++ routes/itemRouter.js
@@ -5,34 +5,88 @@ var Items = require('../lib/Items');
 var SimpleRes = require('../lib/SimpleResponse');
 
 var VAT = require('../resources/VAT');
+var Utils = require('./../lib/Utils');
 
 var savedItems = null;
 
 router.get('/', function ( req, res ) {
 
     if ( savedItems == null )
-        Items.getItems(function ( items ) {
-            if ( !items ) {
-                return res.status(500).json({ Error: 'Could not fetch items from database' });
+        getItems(function ( err, items ) {
+            if ( err ) {
+                return SimpleRes.sendError(req, res, err, 500);
             }
+            SimpleRes.sendSuccess(req, res, items);
 
-
-            //for now this is hardcoded (HARDCODED,FIX)
-            for ( var i = 0; i < items.length; i++ ) {
-                items[ i ].VAT = VAT.vat25; //adds 25% VAT to the item
-            }
-
-            savedItems = items;
-            SimpleRes.sendSuccess(req, res, savedItems);
         });
     else {
         SimpleRes.sendSuccess(req, res, savedItems);
     }
 });
 
-router.get('/:sku', function ( req, res ) {
-    console.log("got one");
+router.get('/search', function ( req, res ) {
+    var sku = req.query.sku;
+    var EAN = req.query.ean;
+
+
+    if ( savedItems == null ) {
+        SimpleRes.sendError(req, res, { Error: 'Need to GET /Items first' }, 500)
+    } else {
+
+        var skuIndex = false;
+        var EANIndex = false;
+
+        var respond = { items: [] };
+
+
+        if ( sku ) {
+            console.log("sku", sku);
+            skuIndex = Utils.indexOf(savedItems, 'sku', sku);
+
+            if ( skuIndex > -1 ) {
+                respond.items.push(savedItems[ skuIndex ]);
+            }
+
+        }
+
+        if ( EAN ) {
+
+            EANIndex = Utils.indexOf(savedItems, 'upc', EAN);
+            console.log("ean", EAN, EANIndex, typeof EAN);
+
+            console.log(savedItems[ 0 ].upc);
+            console.log(typeof savedItems[ 0 ].upc);
+            console.log(savedItems[ 0 ].upc == EAN);
+            console.log(savedItems[ 0 ].upc === EAN);
+
+            if ( EANIndex > -1 ) {
+                respond.items.push(savedItems[ EANIndex ]);
+            }
+
+        }
+
+        SimpleRes.sendSuccess(req, res, respond)
+    }
+
+
 });
+
+function getItems( callback ) {
+    Items.getItems(function ( items ) {
+        if ( !items )
+            return callback({ Error: 'Could not fetch items from database' }, null);
+
+
+        //for now this is hardcoded (HARDCODED,FIX)
+        for ( var i = 0; i < items.length; i++ ) {
+            items[ i ].VAT = VAT.vat25; //adds 25% VAT to the item
+        }
+
+        savedItems = items;
+        callback(null, savedItems);
+    });
+}
+
 
 module.exports = router;
 
