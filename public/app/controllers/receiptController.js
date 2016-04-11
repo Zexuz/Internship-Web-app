8eb2af5baea03dc50b57b4ac6e93b6c4967d9720
Zexuz <Robin.Edbom@gmail.com>
2016-04-11 15:39:33 +0200

    Fixed a error and made mycartVIew responsive

--- public/app/controllers/receiptController.js
+++ public/app/controllers/receiptController.js
@@ -2,7 +2,13 @@ var app = angular.module('myApp');
 
 app.controller('receiptController', ['$scope', 'CartFactory', 'UserFactory', function ($scope, cart, user) {
     $scope.receipt = "Loading receipt";
-    cart.getReceipt(user.getUserInfo().id, function (err,receipt) {
+
+
+    if (user.getUserInfo() == null) {
+        return;
+    }
+
+    cart.getReceipt(user.getUserInfo().id, function (err, receipt) {
         console.log(err);
         $scope.receipt = receipt
     });
